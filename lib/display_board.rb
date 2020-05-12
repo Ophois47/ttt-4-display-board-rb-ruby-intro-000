7388d40b302583071d5caf8c5c142d042f2941e9
David Petnick <dpetnick89@gmail.com>
2020-05-12 00:05:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,6 @@ def display_board
   puts "   |   |   "
 end
 
-board = [" ", " ", " ", "X", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", "", " ", " ", " ", " ", " "]
 
-display_board
+display_board(board)
