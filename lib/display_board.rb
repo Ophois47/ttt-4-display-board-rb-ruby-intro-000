6aca44aae4c973c724ac921c5fb2827c78855948
David Petnick <dpetnick89@gmail.com>
2020-05-12 00:12:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board = "board_empty")
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = ["X", "X", " ", " ", "O", " ", " ", " ", "O"]
+board_empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 display_board(board)
